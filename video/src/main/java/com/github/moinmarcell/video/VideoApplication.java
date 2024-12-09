e94aa45608ed57deb31850e667e8dd9422738344
LostSpezi <marcell.dechant@proton.me>
2024-12-09 19:30:50 +0100

    feat(video): Init video service

---
+++ video/src/main/java/com/github/moinmarcell/video/VideoApplication.java
@@ -0,0 +1,13 @@
+package com.github.moinmarcell.video;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoApplication.class, args);
+	}
+
+}
